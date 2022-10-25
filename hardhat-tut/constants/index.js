519e915732f287f8e805e166d0d7ef5b5022c78f
keneNwobodo <nwobodokenechukwu2@gmail.com>
2022-10-25 16:08:30 +0100

    Deployed ICO app

---
+++ hardhat-tut/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0xF4Cb0e3a171ecCAFD48e8807e9247631eCf88CC9";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
